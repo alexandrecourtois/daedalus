7ac2bf483afcccebcf09cb0b62c2860b06a74051
alexandrecourtois <alexandre@E1404F>
2023-12-30 03:13:20 +0100

    Access Denied

---
+++ cmake-build-Debug/daedalus/CMakeFiles/daedalus.dir/cmake_clean.cmake
@@ -0,0 +1,37 @@
+file(REMOVE_RECURSE
+  "/home/alexandre/Prog/daedalus/cmake-build-Debug/output/daedalus"
+  "/home/alexandre/Prog/daedalus/cmake-build-Debug/output/daedalus.pdb"
+  "CMakeFiles/daedalus.dir/src/AnimatedGIF.cpp.o"
+  "CMakeFiles/daedalus.dir/src/AnimatedGIF.cpp.o.d"
+  "CMakeFiles/daedalus.dir/src/console.cpp.o"
+  "CMakeFiles/daedalus.dir/src/console.cpp.o.d"
+  "CMakeFiles/daedalus.dir/src/dataref.cpp.o"
+  "CMakeFiles/daedalus.dir/src/dataref.cpp.o.d"
+  "CMakeFiles/daedalus.dir/src/gun.cpp.o"
+  "CMakeFiles/daedalus.dir/src/gun.cpp.o.d"
+  "CMakeFiles/daedalus.dir/src/hud.cpp.o"
+  "CMakeFiles/daedalus.dir/src/hud.cpp.o.d"
+  "CMakeFiles/daedalus.dir/src/main.cpp.o"
+  "CMakeFiles/daedalus.dir/src/main.cpp.o.d"
+  "CMakeFiles/daedalus.dir/src/nameable.cpp.o"
+  "CMakeFiles/daedalus.dir/src/nameable.cpp.o.d"
+  "CMakeFiles/daedalus.dir/src/player.cpp.o"
+  "CMakeFiles/daedalus.dir/src/player.cpp.o.d"
+  "CMakeFiles/daedalus.dir/src/rc_engine.cpp.o"
+  "CMakeFiles/daedalus.dir/src/rc_engine.cpp.o.d"
+  "CMakeFiles/daedalus.dir/src/resmgr.cpp.o"
+  "CMakeFiles/daedalus.dir/src/resmgr.cpp.o.d"
+  "CMakeFiles/daedalus.dir/src/sprite.cpp.o"
+  "CMakeFiles/daedalus.dir/src/sprite.cpp.o.d"
+  "CMakeFiles/daedalus.dir/src/stringvector.cpp.o"
+  "CMakeFiles/daedalus.dir/src/stringvector.cpp.o.d"
+  "CMakeFiles/daedalus.dir/src/tools.cpp.o"
+  "CMakeFiles/daedalus.dir/src/tools.cpp.o.d"
+  "CMakeFiles/daedalus.dir/src/viewport.cpp.o"
+  "CMakeFiles/daedalus.dir/src/viewport.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/daedalus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
